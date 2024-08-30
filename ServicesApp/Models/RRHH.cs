064fb0356790430b437ed9731b87956bf2a6dc9b
DanyElAlgo <64906201+DanyElAlgo@users.noreply.github.com>
2024-08-30 19:52:09 -0400

    Recursos humanos update
    si

---
+++ ServicesApp/Models/RRHH.cs
@@ -0,0 +1,7 @@
+public class RRHH{
+    Usuario? datosPersonales {get;set;}
+    string? identificacion {get;set;} //algún otro número de identificación propia de la U
+    object? horario{get;set;} //algo se debe hacer para ajustar esta wea xd
+    
+    
+}
