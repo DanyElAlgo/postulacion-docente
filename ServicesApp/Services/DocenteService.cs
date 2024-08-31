56fb1351f9bf7377cbfa0769e4f2b3be97556624
DanyElAlgo <64906201+DanyElAlgo@users.noreply.github.com>
2024-08-30 20:01:34 -0400

    Docente update
    aaaaaaaaaaaaaaaaaa

--- ServicesApp/Services/DocenteService.cs
+++ ServicesApp/Services/DocenteService.cs
@@ -1,6 +1,9 @@
-public class Docente{
-    public Usuario? datosPersonales {get;set;}
-    string? materia {get;set;} //qué campo de la docencia enseña
-    int experiencia {get;set;} //años de experiencia
-    string? grado {get;set;} //licenciado? Ingeniero? Doctorado?
+public class DocenteService : IDocenteService{
+    public void guardarDatosDocente(){
+
+    }
+    public void verEstado(){
+
+    }
+
 }
---
+++ ServicesApp/Services/IDocenteService.cs
@@ -0,0 +1,4 @@
+public interface IDocenteService{
+    public void guardarDatosDocente();
+    public void verEstado();
+}
